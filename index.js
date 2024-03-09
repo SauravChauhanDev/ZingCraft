d8db60d20b567c09f0b0efb6a11aa989badb6aee
SauravChauhanDev <sauravchauhan.self@gmail.com>
2024-03-10 03:59:23 +0530

    Saurav Commits

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('Please Login!')
+  })
+
+  app.get('/product', (req, res) => {
+    res.send('Product Page!')
+  })
+
+  app.get('/home', (req, res) => {
+    res.send('Home Page')
+  })
+
+  app.get('/logout', (req, res) => {
+    res.send('Logout here!')
+  })
+
+  app.get('/cart', (req, res) => {
+    res.send('Cart is open !')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
